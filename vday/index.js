7e805ba7c367586a061302c241218af99d95a901
abellow247 <121534224+abellow247@users.noreply.github.com>
2023-02-14 03:02:28 -0800

    Add files via upload

---
+++ vday/index.js
@@ -0,0 +1,24 @@
+
+
+
+
+function get () 
+{
+    
+let pass = document.querySelector('.password').value;
+
+if(pass=='bubba')
+    {
+        window.open('new.html');
+    }
+
+
+    
+else{
+    alert('wow, babe .. really?');
+}
+
+
+    
+};
+
